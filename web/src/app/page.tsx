5fa85a8c1b56c5b444757dd2043bc7bdae0fab86
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2024-08-14 16:03:34 -0300

    :construction: feat: added left and right components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,24 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-r-gray-50/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        {/* blut */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 overflow-hidden blur-full"></div>
 
+        {/* stripes */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"></div>
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
